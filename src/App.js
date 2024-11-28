7dbd1c110896007402e798f0d309e8ffddd86e05
n2milmin <nicoleamilmine@gmail.com>
2024-11-28 16:06:27 -0500

    I have most of the app working without errors. The only thing I can't get is the icon Up next is css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Weather } from './componenets/Weather';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+
+  const [ weather, setWeather ] = useState(null)
+  const [ error, setError ] = useState(false)
+  const [ city, setCity ] = useState('')
+
+  const handleSearch = async () => {
+    try{ 
+      const res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c61343ec70b42b60f8ee4989e70b16f0`)
+      setWeather(res.data)
+      setError(false)
+      console.log(res.data)
+    }catch (e) {
+      console.log("Weather fetch error: ", e)
+      setWeather(null)
+      setError(true)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='main'>
+      <header>
+        Check the Weather
       </header>
+
+      <div className='search'>
+        <input 
+          type="text"
+          placeholder='City'
+          value={city}
+          onChange={ c => setCity(c.target.value) }
+        />
+        <button onClick={ () => handleSearch() }>Search</button>
+      </div>
+
+      <div className='warnings'>
+        { error && <p>Weather for { city } could not be found </p> }
+      </div>
+
+      <div >
+      { console.log(weather) }
+        { weather && <Weather weather={ weather }/> }
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/componenets/Weather.js
@@ -0,0 +1,56 @@
+import React from "react";
+import axios from 'axios';
+
+export const Weather = ({ weather }) => {
+    
+    const handleIcons = async () => {
+        try{ 
+            const res = await axios.get(`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`)
+            console.log("fetched icon", res)
+            return res.request.responseURL
+          }catch (e) {
+            console.log("failed fetch icon")
+            return null
+          }
+    }
+
+
+    return(
+        weather ? (
+            <div className="weather_body">
+                <h3 className="city">{ weather && weather.name }</h3>
+
+                <div className="weather-info">
+                    <p>
+                        Current temperature: <strong>{ Math.round(weather['main']['temp'] - 273.15) }&deg;C</strong>
+                    </p>
+                    <p>
+                        Feels like: <strong>{ Math.round(weather.main.feels_like - 273.15) }&deg;C</strong>
+                    </p>
+                    <p>
+                        Max: <strong>{ Math.round(weather.main.temp_max - 273.15) }&deg;C</strong>
+                    </p>
+                    <p>
+                        Min: <strong>{ Math.round(weather.main.temp_min - 273.15) }&deg;C</strong>
+                    </p>
+                </div>
+
+                <div className="sky">
+                    <p className="description" style={{textTransform: 'capitalize'}}>
+                        {  weather.weather[0].description }
+                    </p>
+                    <img 
+                        src={ `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png` } 
+                        alt="no-img"
+                        className="icon"
+                    />
+                </div>
+            </div>
+        ) : (
+            <div>
+                { console.log(weather) }
+                <p>Error</p>
+            </div>
+        )
+    )
+}
